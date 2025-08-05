dac916481448139ba53779fbc768ff91e0bb5406
Lucas Acelino <lucasaacelino@gmail.com>
2025-08-05 20:36:49 -0300

    feat;: adição do arquivo docker-compose.yaml

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def conectar():
     conn = psycopg2.connect(
         host=os.environ["DB_HOST"],
-        port=os.environ["DB_PORT"],
+        # port=os.environ["DB_PORT"],
         dbname=os.environ["DB_NAME"],
         user=os.environ["DB_USER"],
         password=os.environ["DB_PASSWORD"]
